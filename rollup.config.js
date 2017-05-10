6fe5d9ceefda690dc5a8598a85033af3ed9301f4
leofavre <contato@leofavre.com>
2017-05-10 01:02:45 -0300

    minor adjustments

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import nodeResolve from "rollup-plugin-node-resolve";
 
 export default {
-	entry: "./index.js",
-	dest: "./dist/app.js",
-	plugins: [nodeResolve()],
-	format: "es"
+	"entry": "./index.js",
+	"dest": "./dist/app.js",
+	"plugins": [nodeResolve()],
+	"format": "es"
 };
